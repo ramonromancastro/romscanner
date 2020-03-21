cb8039710fbc99a0467f69f06c7e2ee05b7a39ea
Ramón Román Castro <ramonromancastro@gmail.com>
2020-03-21 14:55:23 +0100

    Update romscanner.sh

--- romscanner.sh
+++ romscanner.sh
@@ -26,9 +26,9 @@ VERSION="1.3"
 # Configuration variables
 
 storage_path=/storage
-roms_path=${storage_path}/roms
-thumbnails_path=${storage_path}/thumbnails
-playlists_path=${storage_path}/playlists
+roms_path=
+thumbnails_path=
+playlists_path=
 cores_path=/tmp/cores
 
 # Parameters
@@ -42,26 +42,42 @@ mame_title="Arcade (MAME 2003)"
 mame_dat_url="https://raw.githubusercontent.com/libretro/mame2003-libretro/master/metadata/mame2003.xml"
 mame_dat=mame.dat
 mame_playlists="MAME2003.lpl"
-mame_playlists_path="${playlists_path}/${mame_playlists}"
-mame_thumbnails_path="${thumbnails_path}/MAME2003"
-mame_roms_path="${roms_path}/mame"
-mame_cores_path="${cores_path}/mame2003_libretro.so"
+mame_playlists_path=
+mame_thumbnails_path=
+mame_roms_path=
+mame_cores_path=
 mame_thumbnails_url="https://raw.githubusercontent.com/libretro/libretro-thumbnails/master/MAME"
 
 fba_title="Arcade (FB Alpha - Arcade Games)"
 fba_dat_url="https://raw.githubusercontent.com/libretro/fbalpha/master/dats/FB%20Alpha%20(ClrMame%20Pro%20XML%2C%20Arcade%20only).dat"
 fba_dat=fba.dat
 fba_playlists="FB Alpha - Arcade Games.lpl"
-fba_playlists_path="${playlists_path}/${fba_playlists}"
-fba_thumbnails_path="${thumbnails_path}/FB Alpha - Arcade Games"
-fba_roms_path="${roms_path}/fba"
-fba_cores_path="${cores_path}/fbalpha_libretro.so"
+fba_playlists_path=
+fba_thumbnails_path=
+fba_roms_path=
+fba_cores_path=
 fba_thumbnails_url="https://raw.githubusercontent.com/libretro/libretro-thumbnails/master/FB%20Alpha%20-%20Arcade%20Games"
 
 romsmissing=0
 romsfound=0
 thumbsmissing=0
 thumbsdownloaded=0
+
+fillVariables(){
+  roms_path=${storage_path}/roms
+  thumbnails_path=${storage_path}/thumbnails
+  playlists_path=${storage_path}/playlists
+
+  mame_playlists_path="${playlists_path}/${mame_playlists}"
+  mame_thumbnails_path="${thumbnails_path}/MAME2003"
+  mame_roms_path="${roms_path}/mame"
+  mame_cores_path="${cores_path}/mame2003_libretro.so"
+
+  fba_playlists_path="${playlists_path}/${fba_playlists}"
+  fba_thumbnails_path="${thumbnails_path}/FB Alpha - Arcade Games"
+  fba_roms_path="${roms_path}/fba"
+  fba_cores_path="${cores_path}/fbalpha_libretro.so"
+}
 
 getName () {
   local name=$(sed -n '/<game name="'$1'"/,/<\/description>/p' "$2" | sed -n 's:.*<description>\(.*\)</description>.*:\1:p') 
@@ -98,6 +114,7 @@ checkIfChange (){
 scanarcade () {
 	echo -e "    \e[97mScanning $1 for roms...\e[0m" >&2
 	#if [ ! -f "$1/roms.crc" ]; then touch "$1/roms.crc"; fi
+	unset fullpath
 	for fullpath in $1/$2; do
 		echo -ne "        \e[94m$fullpath\e[0m" >&2 
 		filename=$(basename "$fullpath")
@@ -130,6 +147,9 @@ scanarcade () {
 			romsfound=$((romsfound+1))
 		fi  
 	done
+	if [ -z $fullpath ]; then
+	  echo -e "        \e[33mNo roms found\e[0m" >&2
+	fi
 }
 
 downloadDATs(){
@@ -137,7 +157,7 @@ downloadDATs(){
 		echo "Downloading MAME DAT file..."
 		wget -O $mame_dat $mame_dat_url &> /dev/null
 		if [ $? -ne 0 ]; then
-			echo -e "\033[31mError donwloading MAME DAT file. Try to download manually from [$mame_dat_url] and rename it to [$mame_dat]\e[0m" >&2
+			echo -e "\033[31mError downloading MAME DAT file. Try to download manually from [$mame_dat_url] and rename it to [$mame_dat]\e[0m" >&2
 			rm -f $mame_dat
 			exit 1
 		fi
@@ -146,7 +166,7 @@ downloadDATs(){
 		echo "Downloading FBA DAT file..."
 		wget -O $fba_dat $fba_dat_url &> /dev/null
 		if [ $? -ne 0 ]; then
-			echo -e "\033[31mError donwloading FBA DAT file. Try to download manually from [$fba_dat_url] and rename it to [$fba_dat]\e[0m" >&2
+			echo -e "\033[31mError downloading FBA DAT file. Try to download manually from [$fba_dat_url] and rename it to [$fba_dat]\e[0m" >&2
 			rm -f $fba_dat
 			exit 1
 		fi
@@ -274,9 +294,6 @@ while [[ $# -gt 0 ]]; do
 			;;
 		-s|--storage)
 			storage_path=$2
-			roms_path=${storage_path}/roms
-			thumbnails_path=${storage_path}/thumbnails
-			playlists_path=${storage_path}/playlists
 			shift
 			;;
 		-c|--cores)
@@ -291,6 +308,17 @@ while [[ $# -gt 0 ]]; do
 	shift
 done
 
+## Fill variables
+
+fillVariables
+
+# Test dirs
+
+if [ ! -d "$roms_path" ]; then echo -e "\e[33m${roms_path} not found!\e[0m"; exit 1; fi
+if [ ! -d "$thumbnails_path" ]; then echo -e "\e[33m${thumbnails_path} not found!\e[0m"; exit 1; fi
+if [ ! -d "$playlists_path" ]; then echo -e "\e[33m${playlists_path} not found!\e[0m"; exit 1; fi
+if [ ! -d "$cores_path" ]; then echo -e "\e[33m${cores_path} not found!\e[0m"; exit 1; fi
+
 # Download DATs files
 
 downloadDATs
@@ -300,14 +328,8 @@ downloadDATs
 rm romscanner.unknown -f > /dev/null
 rm romscanner.ok -f > /dev/null
 
-# Test dirs
-
-if [ ! -d "$roms_path" ]; then echo -e "\e[33m${roms_path} not found!\e[0m"; exit 1; fi
-if [ ! -d "$thumbnails_path" ]; then echo -e "\e[33m${thumbnails_path} not found!\e[0m"; exit 1; fi
-if [ ! -d "$playlists_path" ]; then echo -e "\e[33m${playlists_path} not found!\e[0m"; exit 1; fi
-if [ ! -d "$cores_path" ]; then echo -e "\e[33m${cores_path} not found!\e[0m"; exit 1; fi
-
 # MAME
+
 echo -e "\e[97m${mame_title}\e[0m"
 if [ -d "$mame_roms_path" ]; then
 	change=$(checkIfChange "$mame_roms_path")
@@ -325,6 +347,7 @@ else
 fi
 
 # FBA
+
 echo -e "\e[97m${fba_title}\e[0m"
 if [ -d "$fba_roms_path" ]; then
 	change=$(checkIfChange "$fba_roms_path")
